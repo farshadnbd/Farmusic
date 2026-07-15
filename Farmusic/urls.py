dfdcaa42fbe1928b5713aad0e01a68705b7b5c26
Farshad <farshadnbd16@gmail.com>
2026-07-15 20:05:47 +0330

    fix sitemap

--- Farmusic/urls.py
+++ Farmusic/urls.py
@@ -3,38 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-from django.contrib.sitemaps.views import sitemap
-from music.sitemaps import (
-MusicSitemap,
-ArtistSitemap,
-AlbumSitemap,
-)
-
-sitemaps = {
-'music': MusicSitemap,
-'artist': ArtistSitemap,
-'album': AlbumSitemap,
-}
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('', include('music.urls')),
-path('accounts/', include('accounts.urls')),
-path('payments/', include('payments.urls')),
-path('admin-dashboard/', include('dashboard.urls')),
-path('51475603.txt', lambda r: HttpResponse("51475603", content_type="text/plain")),
-
-path(
-    'sitemap.xml',
-    sitemap,
-    {'sitemaps': sitemaps},
-    name='django.contrib.sitemaps.views.sitemap'
-),
-
+    path('admin/', admin.site.urls),
+    path('', include('music.urls')), # 👈 تمام مسیرهای موزیک، سایتمپ و ربات اینجاست
+    path('accounts/', include('accounts.urls')),
+    path('payments/', include('payments.urls')),
+    path('admin-dashboard/', include('dashboard.urls')),
+    path('51475603.txt', lambda r: HttpResponse("51475603", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
     )
